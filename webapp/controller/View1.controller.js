d246a37ae4c70626a0ab2569cab8bebb48e21693
Girish Hc <girish.hc@castaliaz.co.in>
2026-04-08 05:38:11 +0000

    NCL:Alignment changes

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -5,6 +5,112 @@ sap.ui.define([
 
     return Controller.extend("zfzpackingslip.controller.View1", {
         onInit() {
-        }
+            var that=this;
+            this.setDefaultSettings();
+            this.setHeaderModel();
+        },
+        setDefaultSettings:function(){
+                var JsonMModel = new sap.ui.model.json.JSONModel({
+                        "HeaderEditableFileds": true, 
+                        "SaveBtn": "true"      
+                })
+                this.getView().setModel(JsonMModel, "LocalViewSettings");
+        },
+        setHeaderModel:function(){
+               var JsonMModel = new sap.ui.model.json.JSONModel({
+                        "HeaderEditableFileds": true, 
+                        "SaveBtn": "true"      
+                })
+                this.getView().setModel(JsonMModel, "HeaderDataModel");
+        },
+        OnChangeOrderNo:function(oEvent){
+
+        },
+        OnChangeOrderNo:function(oEvent){
+
+        },
+        OnChangeOrderNo:function(oEvent){
+
+        },
+        OnChangeOrderNo:function(oEvent){
+
+        },
+        OnChangeOrderNo:function(oEvent){
+
+        },
+        OnChangeOrderNo:function(oEvent){
+
+        },
+        OnChangeOrderNo:function(oEvent){
+
+        },
+        OnChangeOrderNo:function(oEvent){
+
+        },
+        OnChangeOrderNo:function(oEvent){
+
+        },
+        OnChangeOrderNo:function(oEvent){
+
+        },
+        OnChangeOrderNo:function(oEvent){
+
+        },
+        OnChangeOrderNo:function(oEvent){
+
+        },
+        OnChangeOrderNo:function(oEvent){
+
+        },
+        OnChangeOrderNo:function(oEvent){
+
+        },
+        OnChangeOrderNo:function(oEvent){
+
+        },
+        OnChangeOrderNo:function(oEvent){
+
+        },
+        OnChangeOrderNo:function(oEvent){
+
+        },
+        OnChangeOrderNo:function(oEvent){
+
+        },
+        OnChangeOrderNo:function(oEvent){
+
+        },
+        OnChangeOrderNo:function(oEvent){
+
+        },
+        OnChangeOrderNo:function(oEvent){
+
+        },
+        OnChangeOrderNo:function(oEvent){
+
+        },
+        OnChangeOrderNo:function(oEvent){
+
+        },
+        OnChangeOrderNo:function(oEvent){
+
+        },
+        OnChangeOrderNo:function(oEvent){
+
+        },
+        onClickDisplay:function(){
+
+        },
+        onSave:function(){
+
+        },
+        onCancel:function(){
+
+        },
+        onReject:function(){
+
+        },
+
+
     });
 });
